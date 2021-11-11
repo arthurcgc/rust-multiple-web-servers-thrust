8447bcb150d335a2ea436af40391c29134521470
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2021-11-11 15:20:26 -0300

    multiple servers hello world

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use log::info;
+use thruster::{async_middleware, middleware_fn};
+use thruster::{App, BasicContext as Ctx, Request, Server, ThrusterServer};
+use thruster::{MiddlewareNext, MiddlewareResult};
+
+#[middleware_fn]
+async fn plaintext(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    let val = "Hello, World!";
+    context.body(val);
+    Ok(context)
+}
+
+#[middleware_fn]
+async fn noop(context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    Ok(context)
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    info!("Starting server...");
+
+    let mut app = App::<Request, Ctx, ()>::new_basic();
+
+    app.get("/plaintext", async_middleware!(Ctx, [plaintext]));
+
+    let server = Server::new(app);
+    tokio::spawn(server.build("0.0.0.0", 4321));
+
+    let mut healthcheck = App::<Request, Ctx, ()>::new_basic();
+
+    healthcheck.get("/healthz", async_middleware!(Ctx, [noop]));
+
+    let healthcheck_server = Server::new(healthcheck);
+    tokio::spawn(healthcheck_server.build("0.0.0.0", 8080));
+
+    futures::future::pending().await
+}
